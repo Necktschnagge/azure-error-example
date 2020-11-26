44717688a8692d61e2b5080e6567f0f64e9b6707
Maximilian Starke <maximilian.starke@medionmail.com>
2020-11-26 21:35:23 +0100

    try this

--- src/devices_2.h
+++ src/devices_2.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "devices_1.h"
-#include "driver_distcon.h"
 
 #include <array>
 
@@ -16,11 +15,9 @@ class devices { // refactor-ready
 template<>
 class devices<2> : public devices<1> {
 	inline static devices<2>* _instance{ nullptr };
-	distcon_device _distcon{ std::make_unique<inverse_iopin>(pin_id::distcon_data), std::make_unique<inverse_iopin>(pin_id::distcon_clock) };
 protected:
 	devices() { _instance = this; }
 public:
-	distcon_device& distcon() { return _distcon; }
 	static devices<2>* instance() { return _instance; }
 };
 
